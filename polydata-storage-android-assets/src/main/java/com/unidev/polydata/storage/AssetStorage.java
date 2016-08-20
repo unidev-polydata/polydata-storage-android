39678b8d5d052bf6ba64bdca83a11f29a0759505
Denis O <denis@universal-development.com>
2016-08-20 21:18:22 +0300

    Assets storage template

---
+++ polydata-storage-android-assets/src/main/java/com/unidev/polydata/storage/AssetStorage.java
@@ -0,0 +1,21 @@
+package com.unidev.polydata.storage;
+
+import com.unidev.polydata.domain.Poly;
+
+import java.util.Collection;
+
+/**
+ * Storage backed by assets json files
+ */
+public class AssetStorage implements PolyStorage {
+
+    @Override
+    public Poly fetchById(String s) {
+        return null;
+    }
+
+    @Override
+    public Collection<Poly> list() {
+        return null;
+    }
+}
