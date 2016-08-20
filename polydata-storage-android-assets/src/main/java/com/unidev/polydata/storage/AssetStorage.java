42e9d2b72ebdce8282fe722f661749220ab9da37
Denis O <denis@universal-development.com>
2016-08-20 22:55:19 +0300

    object mapper initialization

--- polydata-storage-android-assets/src/main/java/com/unidev/polydata/storage/AssetStorage.java
+++ polydata-storage-android-assets/src/main/java/com/unidev/polydata/storage/AssetStorage.java
@@ -1,5 +1,7 @@
 package com.unidev.polydata.storage;
 
+import android.content.Context;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.unidev.polydata.domain.Poly;
 
 import java.util.Collection;
@@ -8,6 +10,16 @@ import java.util.Collection;
  * Storage backed by assets json files
  */
 public class AssetStorage implements PolyStorage {
+
+    public static ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+    }
+
+    public void load(Context context) {
+
+    }
 
     @Override
     public Poly fetchById(String s) {
