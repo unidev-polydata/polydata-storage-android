ea853f9c238123847fc65d1776e90771e3895ff0
Denis O <denis@universal-development.com>
2016-08-21 15:05:40 +0300

    Android test app. testing for asset loading

---
+++ polydata-storage-android-test/src/main/java/com/unidev/app/testapp/activity/MainFragment.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2015,2016 Denis O <denis@universal-development.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.unidev.app.testapp.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.unidev.app.testapp.R;
+
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main, container, false);
+
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        return view;
+    }
+
+
+
+}
